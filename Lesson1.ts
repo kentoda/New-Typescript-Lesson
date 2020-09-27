99db4da711e95b42978b77ce5a9a50ed1d5d99cf
kentoda <kent19950811@gmail.com>
2020-09-27 13:48:57 +0900

    add

---
+++ Lesson1.ts
@@ -0,0 +1,13 @@
+let hasValue: boolean = true;
+// hasValue = true;と記述しても型推論してくれる
+
+// number型は整数も浮動小数点も、-も同一
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
+
+// string型も'', "", `` 同一
+let single: string = 'hello';
+let double: string = "hello";
+let back: string = `hello`;
+
